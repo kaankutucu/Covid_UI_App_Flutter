462a6d0dd2445c30fd0de5aff516296aa133e7d9
kaankutucu <kkutucu@gmail.com>
2020-06-13 17:28:43 +0300

    New Cases Finished

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:coviduiapp/constants.dart';
+import 'package:coviduiapp/widgets/weekly_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -7,69 +8,152 @@ class DetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildDetailsAppBar(context),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20),
-        child: Column(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 25,
-              ),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                    offset: Offset(0, 21),
-                    blurRadius: 53,
-                    color: Colors.black.withOpacity(0.05),
-                  ),
-                ],
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  buildTitleWithMoreIcon(),
-                  SizedBox(height: 15,),
-                  buildCaseNumber(context),
-                  SizedBox(height: 15,),
-                  Text(
-                    "From Health Center",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w200,
-                      color: kTextMediumColor,
-                      fontSize: 16,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 25,
+                ),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0, 21),
+                      blurRadius: 53,
+                      color: Colors.black.withOpacity(0.05),
                     ),
-                  ),
-                ],
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    buildTitleWithMoreIcon(),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    buildCaseNumber(context),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      "From Health Center",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w200,
+                        color: kTextMediumColor,
+                        fontSize: 16,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    WeeklyChart(),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        buildInfoTextWithPercentage(
+                          percentage: "6.43",
+                          title: "From Last Week",
+                        ),
+                        buildInfoTextWithPercentage(
+                          percentage: "6.43",
+                          title: "From Last Week",
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0, 21),
+                      blurRadius: 54,
+                      color: Colors.black.withOpacity(0.05),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          "Global Menu",
+                          style: TextStyle(
+                            fontSize: 15,
+                          ),
+                        ),
+                        SvgPicture.asset("assets/icons/more.svg"),
+                      ],
+                    ),
+                    SizedBox(height: 20,),
+                    SvgPicture.asset("assets/icons/map.svg"),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
+  RichText buildInfoTextWithPercentage({String title, String percentage}) {
+    return RichText(
+      text: TextSpan(
+        children: [
+          TextSpan(
+            text: "$percentage% \n",
+            style: TextStyle(
+              fontSize: 20,
+              color: kPrimaryColor,
+            ),
+          ),
+          TextSpan(
+            text: title,
+            style: TextStyle(
+              color: kTextMediumColor,
+              height: 1.5,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
   Row buildCaseNumber(BuildContext context) {
     return Row(
-                  children: <Widget>[
-                    Text(
-                      "547 ",
-                      style: Theme.of(context)
-                          .textTheme
-                          .display3
-                          .copyWith(color: kPrimaryColor, height: 1.2),
-                    ),
-                    Text(
-                      "5.9%",
-                      style: TextStyle(
-                        color: kPrimaryColor,
-                      ),
-                    ),
-                    SvgPicture.asset("assets/icons/increase.svg"),
-                  ],
-                );
+      children: <Widget>[
+        Text(
+          "547 ",
+          style: Theme.of(context)
+              .textTheme
+              .display3
+              .copyWith(color: kPrimaryColor, height: 1.2),
+        ),
+        Text(
+          "5.9% ",
+          style: TextStyle(
+            color: kPrimaryColor,
+          ),
+        ),
+        SvgPicture.asset("assets/icons/increase.svg"),
+      ],
+    );
   }
   Row buildTitleWithMoreIcon() {
     return Row(
